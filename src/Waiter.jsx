77c1d6506d32b9ffa0cc19f8ceacea877354882a
mahmudul-Hasan-2 <mahmudul5709@gmail.com>
2026-03-14 17:10:33 +0600

    I has explore more about Destructuring to display many of information

--- src/Waiter.jsx
+++ src/Waiter.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 
 export function Waiter({ waiter }) {
-  const {name, phone, email} = waiter;
+  const {name, phone, email, username, address:{city, street}} = waiter;
   
   return (
     <div className='card'>
-      <h2>{name}</h2>
-      <h2>{phone}</h2>
-      <p>{email}</p>
+      <h2>Name: {name}</h2>
+      <h2>Username: {username}</h2>
+      <h5>Phone Number: {phone}</h5>
+      <p>Email: {email}</p>
+      <p>Address: {street}, {city}</p>
     </div>
   )
 }
